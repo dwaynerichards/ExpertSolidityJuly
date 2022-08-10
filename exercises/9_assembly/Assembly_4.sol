878a7d9b19b546094c1ab8d6b9fc1ffafa5923c4
dwayneRichards <dwaynerichards@gmail.com>
2022-08-10 16:44:27 -0400

    resolved imports from OZ

--- exercises/9_assembly/Assembly_4.sol
+++ exercises/9_assembly/Assembly_4.sol
@@ -1,26 +1,18 @@
 // I AM NOT DONE
 
 pragma solidity ^0.8.4;
+
 contract Scope {
-
     uint public count = 10;
-    
-    function increment(uint numb) public {        
 
-        // Modify state of the count from within 
+    function increment(uint numb) public {
+        // Modify state of the count from within
         // the assembly segment
-        assembly {                                 
-          //load into stack with .slot
-          let c := sload(count.slot)
-          //store value at .slot location in store, after incrementing by numb
-          sstore(
-            count.slot,
-            add(numb, c)
-            )
-          //increment
-          //store 
+        assembly {
+            //load into stack with .slot
+            let c := sload(count.slot)
+            //store value at .slot location in store, after incrementing by numb
+            sstore(count.slot, add(numb, c))
         }
-    }    
+    }
 }
-
-                      
