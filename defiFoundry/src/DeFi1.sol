ce55993dd204c907ef129cfe2a1956c6fe1aaec4
dwayneRichards <dwaynerichards@gmail.com>
2022-08-18 19:45:45 -0400

    3 test failing

--- defiFoundry/src/DeFi1.sol
+++ defiFoundry/src/DeFi1.sol
@@ -1,19 +1,18 @@
 //SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.13;
 
 import "./Token.sol";
 
 contract DeFi1 {
-    uint256 initialAmount = 0;
-    uint256 numInvestors;
-    mapping(address => bool) investors;
-    uint256 rewardRate;
-    uint256 blockNumber;
-    Token public token;
+    uint256 public immutable initialAmount;
+    uint256 public numInvestors;
+    uint256 public rewardRate;
+    uint256 public blockNumber;
+    mapping(address => bool) public investors;
+    Token public immutable token;
 
     constructor(uint256 _initialAmount, uint256 _rewardRate) {
-        initialAmount = initialAmount;
+        initialAmount = _initialAmount;
         token = new Token(_initialAmount);
         blockNumber = block.number;
         rewardRate = _rewardRate;
@@ -21,6 +20,7 @@ contract DeFi1 {
 
     function claimTokens() public {
         require(investors[msg.sender], "notInvestor");
+        require(block.number > blockNumber, "tooSoon");
         token.transfer(msg.sender, _calculatePayout());
     }
 
@@ -29,12 +29,22 @@ contract DeFi1 {
 @return uint256 payout to investors
  */
     function _calculatePayout() internal returns (uint256) {
-        if (block.number >= block.number + 1000) {
-            blockNumber = block.number;
-            rewardRate - 100;
+        uint256 blockDiff = block.number - blockNumber;
+        if (blockDiff > 1000) {
+            (blockNumber, rewardRate) = _calculateRate(blockDiff);
         }
         uint256 blockReward = block.number % rewardRate;
         return (initialAmount / numInvestors) * blockReward;
+    }
+
+    function _calculateRate(uint256 _blockDiff)
+        internal
+        view
+        returns (uint256 _blockNum, uint256 _rewardRate)
+    {
+        uint256 rate = _blockDiff / 1000;
+        uint256 rewardDeduction = rate * 100;
+        return (block.number, (rewardRate - rewardDeduction));
     }
 
     function addInvestor(address _investor) public {
--- defiFoundry/test/Contract.t.sol
+++ defiFoundry/test/Contract.t.sol
@@ -18,30 +18,60 @@ contract ContractTest is Test {
     User internal bob;
     User internal chloe;
     uint256 initialAmount = 1000;
-    uint256 blockReward = 5;
+    uint256 blockRewardRate = 1000;
 
     function setUp() public {
-        defi = new DeFi1(initialAmount, blockReward);
+        defi = new DeFi1(initialAmount, blockRewardRate);
         token = Token(defi.token.address);
         alice = new User();
         bob = new User();
         chloe = new User();
     }
 
-    function testInitialBalance() public {
-        
+///functions prefixed with testFail should revert, or it will fail
+    function testInitialBalance() public view {
+       assert(initialAmount == defi.initialAmount());
+    }
+    function testInitRewardRate() public view {
+       assert(blockRewardRate == defi.rewardRate());
+    }
+    function testFailInitialBalance() public view {
+       assert(initialAmount + 1 == defi.initialAmount());
+    }
+    function testFailInitRewardRate() public view {
+       assert(blockRewardRate +1 == defi.rewardRate());
+    }
+    function testInitNumInvestors() public view {
+        assert(defi.numInvestors() == 0);
+    }
+    function testFailInitNumInvestors() public view {
+        assert(defi.numInvestors() == 1);
     }
 
     function testAddInvestor() public {
         defi.addInvestor(address(alice));
-        assert(defi.investors(0) == address(alice));
+        assert(defi.investors(address(alice)) == true);
+        assert(defi.numInvestors() == 1);
+    }
+    function tesFailAddInvestor() public {
+        defi.addInvestor(address(alice));
+        assert(defi.investors(address(alice)) == false);
+        assert(defi.numInvestors() == 0);
     }
 
-    function testClaim() public {
+    function testCanClaim() public {
         defi.addInvestor(address(alice));
         defi.addInvestor(address(bob));
-        vm.prank(address(alice));
-        vm.roll(1);
+        vm.prank(address(alice));//alice = msg.sender
+        vm.roll(1);//blocknumer setto 1
+        defi.claimTokens();
+    }
+    function testCannotClaim() public {
+        vm.expectRevert(bytes("notInvestor"));
+        defi.addInvestor(address(alice));
+        defi.addInvestor(address(bob));
+        vm.prank(address(chloe));//alice = msg.sender
+        vm.roll(1);//blocknumer setto 1
         defi.claimTokens();
     }
 
@@ -51,11 +81,27 @@ contract ContractTest is Test {
     }
 
     function testAddingManyInvestors() public {
+        defi.addInvestor(address(alice));
+        assert(defi.investors(address(alice)) == true);
+        assert(defi.numInvestors() == 1);
+        defi.addInvestor(address(bob));
+        assert(defi.investors(address(bob)) == true);
+        assert(defi.numInvestors() == 2);
+        defi.addInvestor(address(chloe));
+        assert(defi.investors(address(chloe)) == true);
+        assert(defi.numInvestors() == 3);
 
     }
 
     function testAddingManyInvestorsAndClaiming() public {
-
+        defi.addInvestor(address(alice));
+        assert(defi.investors(address(alice)) == true);
+        assert(defi.numInvestors() == 1);
+        defi.addInvestor(address(bob));
+        assert(defi.investors(address(bob)) == true);
+        assert(defi.numInvestors() == 2);
+        assert(defi.investors(address(chloe)) == true);
+        assert(defi.numInvestors() == 3);
     }
 
 }
